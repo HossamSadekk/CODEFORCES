968ff433c5cd0e446969d21f3b682dc7f89c97ef
HossamSadek <hosssamsadek@gmail.com>
2020-07-02 01:35:09 +0200

    solution of team problem

---
+++ Team.java
@@ -0,0 +1,33 @@
+
+package team;
+import java.util.Scanner;
+public class Team {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        int solve=0;int total=0;
+        
+        int Problems = input.nextInt();
+        int size=Problems*3;
+        int [] arr = new int[size];
+       for(int i=0;i<arr.length;i++)
+       {
+           arr[i]=input.nextInt();
+       
+        for(int j=0;j<3;j++)
+       {
+           total+=arr[j];
+           total+=arr[j++];
+       }
+       }
+        
+        System.out.println(""+total);
+        
+        
+        
+        
+       // if(arr1[0]=1&&arr2[1]=1){solve++;}
+    }
+    
+}
