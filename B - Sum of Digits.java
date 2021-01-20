7350a38c68fa52743abb1c499de3a559f9a94acb
Hossam <hosssamsadek@gmail.com>
2021-01-20 14:17:10 +0200

    add solution of  B-Sum of Digits problem

---
+++ B - Sum of Digits.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class BSumOfDigits {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String n = in.next();
+        int x=n.length();
+        long m=0;
+        int numOfspels=0;
+        if(n.length()==1){System.out.println("0");
+        return;}
+        
+        
+        while(n.length()!=1)
+        {
+            for(int i=0;i<n.length();i++)
+            {
+                m+=Integer.parseInt(String.valueOf(n.charAt(i)));
+            }
+            n=String.valueOf(m);
+            m=0;
+            numOfspels++;
+        }
+        System.out.println(numOfspels);
+    }
+    
+}
